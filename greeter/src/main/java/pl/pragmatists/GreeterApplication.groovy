68dd9eb4ffd71b83d30959296bab5bb95369abe3
Pawel Barszcz <pawelbarszcz@gmail.com>
2015-10-07 11:13:32 +0200

    [greeter] properties provided by @ConfigurationProperties

--- greeter/src/main/java/pl/pragmatists/GreeterApplication.groovy
+++ greeter/src/main/java/pl/pragmatists/GreeterApplication.groovy
@@ -1,31 +1,39 @@
 package pl.pragmatists
 
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @SpringBootApplication
+@EnableConfigurationProperties(GreetingTextProperties)
 class GreeterApplication {
 
     static void main(String[] args) {
         SpringApplication.run GreeterApplication, args
     }
 
-    @Value('${greeting.text.start}')
-    String greetingStart
-
-    @Value('${greeting.text.end}')
-    String greetingEnd
+    @Autowired
+    GreetingTextProperties greetingTextProperties
 
     @RequestMapping('/{who}')
     def greet(@PathVariable who) {
         return [
-            greeting: "${greetingStart}${who}${greetingEnd}".toString(),
+            greeting: "${greetingTextProperties.start}${who}${greetingTextProperties.end}".toString(),
             timestamp: new Date()
         ]
     }
+}
+
+@ConfigurationProperties(prefix = 'greeting.text')
+class GreetingTextProperties {
+
+    String start;
+
+    String end;
 }
